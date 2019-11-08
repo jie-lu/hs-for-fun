8e094835759c9adb0876ab93fead660d552ded52
Jie Lu <jie.lu@aprimo.com>
2019-11-08 16:59:07 +0800

    Temp commit for rotation touch.

--- src/app/hand-container/hand-container.component.ts
+++ src/app/hand-container/hand-container.component.ts
@@ -18,13 +18,20 @@ export class HandContainerComponent implements OnInit, OnDestroy {
   private _onDestroy$ = new Subject<void>();
   private _touchStartPt: Point = null;
   private _fanAngle: number = 0;
+  private _sideOfPointToLine: number = 0;
+  private _containerCenter: Point;
 
   constructor(private el:ElementRef, private changeDetectorRef: ChangeDetectorRef) {}
 
   private initContainerSize() {
+    let containerRect = this.el.nativeElement.getBoundingClientRect();
     this._calculator.containerSize = {
       width: this.el.nativeElement.clientWidth,
       height: this.el.nativeElement.clientHeight
+    };
+    this._containerCenter = {
+      x: this.el.nativeElement.clientWidth / 2,
+      y: this.el.nativeElement.clientHeight / 2
     };
   }
 
@@ -54,23 +61,56 @@ export class HandContainerComponent implements OnInit, OnDestroy {
     this.updateCards(true);
   }
 
-  private beginFanning(x: number, y: number) {
-    this._touchStartPt = {
-      x: x,
-      y: y
-    };
+  private beginFanning(pt: Point) {
+    this._touchStartPt = pt;
   }
 
-  private performFanning(x: number, y: number) {
+  private distance(pt1: Point, pt2: Point) {
+    return Math.sqrt((pt1.x - pt2.x) * (pt1.x - pt2.x) + (pt1.y - pt2.y) * (pt1.y - pt2.y));
+  }
+
+  private angle(vertex: Point, pt2: Point, pt3: Point) {
+    let d12 = this.distance(vertex, pt2);
+    let d13 = this.distance(vertex, pt3);
+    let d23 = this.distance(pt2, pt3);
+    return Math.acos((d12 * d12 + d13 * d13 - d23 * d23) / (2 * d12 * d13)) / Math.PI * 180;
+  }
+
+  private sideOfPointToLine(a: Point, b: Point, pt: Point){
+    return ((b.x - a.x)*(pt.y - a.y) - (b.y - a.y) * (pt.x - a.x));
+  }
+
+  private performFanning(pt: Point) {
     if(this._touchStartPt === null) return;
 
-    let dx = x - this._touchStartPt.x;
-    this._fanAngle = 360 / (this._calculator.containerSize.width / 2) * dx
+    //let dx = x - this._touchStartPt.x;
+    //this._fanAngle = 360 / (this._calculator.containerSize.width / 2) * dx
+    let angle = this.angle(this._containerCenter, this._touchStartPt, pt);
+    let pointSide = this.sideOfPointToLine(this._touchStartPt, this._containerCenter, pt);
+    if(this._sideOfPointToLine === 0) {
+      this._sideOfPointToLine = pointSide;
+    } 
+    
+    if(this._sideOfPointToLine > 0) {
+      angle = -angle;
+    }
+    
+    if(pointSide * this._sideOfPointToLine < 0) {
+      if(this._sideOfPointToLine > 0) {
+        angle = - (360 - angle);
+      } else {
+        angle = (360 - angle);
+      }
+    }
+    this._fanAngle = angle;
     this.updateCards(false, null, this._fanAngle);
+
+    
   }
 
   private stopFanning() {
     this._touchStartPt = null;
+    this._sideOfPointToLine = 0;
   }
 
   @HostListener('touchstart', ['$event'])
@@ -83,7 +123,7 @@ export class HandContainerComponent implements OnInit, OnDestroy {
     this.resetFan();
 
     let touch = ev.changedTouches[0];
-    this.beginFanning(touch.screenX, touch.screenY);
+    this.beginFanning({x: touch.clientX, y: touch.clientY});
   }
 
   @HostListener('touchmove', ['$event'])
@@ -94,7 +134,7 @@ export class HandContainerComponent implements OnInit, OnDestroy {
     if(ev.changedTouches.length !== 1) return;
 
     let touch = ev.changedTouches[0];
-    this.performFanning(touch.screenX, touch.screenY);
+    this.performFanning({x: touch.clientX, y: touch.clientY});
   }
 
   @HostListener('touchend', ['$event'])
@@ -118,12 +158,12 @@ export class HandContainerComponent implements OnInit, OnDestroy {
     ev.preventDefault();
 
     this.resetFan();
-    this.beginFanning(ev.screenX, ev.screenY);
+    this.beginFanning({ x: ev.clientX, y: ev.clientY });
   }
 
   @HostListener('mousemove', ['$event'])
   onMouseMove(ev) {
-    this.performFanning(ev.screenX, ev.screenY);
+    this.performFanning({ x: ev.clientX, y: ev.clientY });
   }
 
   @HostListener('mouseup')
